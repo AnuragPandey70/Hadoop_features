436599f5da747f6ada72c8ad55ae68f9510a9cc1
AnuragPandey70 <anuragpandey030297@gmail.com>
2024-09-16 00:53:09 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,20 @@
+#Variable -- Containers to store data
+#Keywords -- Reserved keywords in python
+#Identifiers -- Class/Function/Variable name
+#To get list in python we use keyword module
+import keyword as k
+print(k.kwlist)
+
+
+a ='''Anurag
+is a good boy'''
+b = 345
+c = 45.32
+d = False
+e = None
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
